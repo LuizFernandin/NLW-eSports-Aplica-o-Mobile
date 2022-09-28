db5855bb62c6e03d192725133af95522bf7bc616
LuizFernandin <luizx2012x@gmail.com>
2022-09-28 13:18:39 -0300

    Final da NLW eSports

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,22 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { Home } from '../sreens/Home';
+import { Game } from '../sreens/Game';
+
+const { Navigator, Screen } = createNativeStackNavigator();
+
+export function AppRoutes() {
+    return (
+        <Navigator screenOptions={{ headerShown: false }}>
+        <Screen 
+            name='home'
+            component={Home}
+        />
+
+        <Screen 
+            name='game'
+            component={Game}
+        />
+        </Navigator>
+    )
+}
